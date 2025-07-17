d477840bcd9e5006912ad1be52a6f79489730a86
shaktisaini <shaktisaini@smartdatainc.net>
2025-07-17 11:12:16 +0530

    changes templates

--- api/services/cron.services.js
+++ api/services/cron.services.js
@@ -1,29 +1,27 @@
 const cron = require('node-cron');
 const Client = require('../models/clientModel'); // Adjust to your path
-const UploadedDocument = require('../models/uploadDocuments');
+const documentRequests = require('../models/documentRequest');
 const mailServices = require('../services/mail.services');
 const twilioServices = require('../services/twilio.services');
 const mongoose = require('mongoose')
 
 
 async function scheduleDailyReminder(expression, clientIds, templateId, notifyMethod, documentId) {
-  const document = await UploadedDocument.findOne({ request: documentId });
+  const document = await documentRequests.findOne({ _id: documentId });
   if (!document) {
     console.error(`[Reminder Cron] Document not found for ID: ${documentId}`);
     return;
   }
   cron.schedule(expression, async () => {
     try {
-     const { name: docName, link, dueDate } = document;
+      const { name: doctitle, requestLink, dueDate } = document;
       const clients = await Client.find({ _id: { $in: clientIds } });
       for (const client of clients) {
         const { _id: clientId, email, name } = client;
-
         if (notifyMethod[0] === "email") {
-          const subject = `Reminder: Upload ${docName}`;
-          const msg = `Dear ${name},\n\nPlease upload the document "${docName}" by ${dueDate?.toDateString() || 'the due date'}.\n\nClick here to upload: ${link}`;
-
-          await mailServices.sendEmailRemainder(email, subject, link, name, msg);
+          const subject = `Reminder: Upload ${doctitle}`;
+          const msg = `Dear ${name},\n\nPlease upload the document "${doctitle}" by ${dueDate?.toDateString() || 'the due date'}.\n\nClick here to upload: ${requestLink}`;
+          await mailServices.sendEmailRemainder(email, subject, requestLink, name, msg);
           console.log(`[Email Reminder] Sent to ${email}`);
         } else if (notifyMethod === "sms") {
           await twilioServices(clientId, templateId, documentId);
