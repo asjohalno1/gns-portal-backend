09f8ffd25e530bb3b54183af52222f90a294b4e2
shaktisaini <shaktisaini@smartdatainc.net>
2025-08-13 11:20:38 +0530

    add auth

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -18,59 +18,59 @@ const auth = require('../middleware/auth');
 
 module.exports = function (app, validator) {
   /** Category Routes's starts */
-  app.post('/api/category/add', validator.body(adminModel.addCategory), adminCntrl.addCategory)
-  app.get('/api/category/getAllcategory', adminCntrl.getAllCategory)
+  app.post('/api/category/add',auth, validator.body(adminModel.addCategory), adminCntrl.addCategory)
+  app.get('/api/category/getAllcategory',auth, adminCntrl.getAllCategory)
   /** Category Routes's ends */
 
   /** SubCategory Routes's starts */
   app.post('/api/subcategory/add', auth, validator.body(adminModel.addSubCategory), adminCntrl.addSubCategory)
-  app.get('/api/subcategory/getAllSubCategory', adminCntrl.getAllSubCategory)
-  app.get('/api/subcategory/getAllSubCategoryByCategory/:id', adminCntrl.getAllSubCategoryByCategory)
+  app.get('/api/subcategory/getAllSubCategory',auth, adminCntrl.getAllSubCategory)
+  app.get('/api/subcategory/getAllSubCategoryByCategory/:id',auth,adminCntrl.getAllSubCategoryByCategory)
   /** SubCategory Routes's ends */
 
 
   /*** Client Routes's starts */
-  app.post('/api/client/add', validator.body(adminModel.addClient), adminCntrl.addClient)
-  app.get('/api/client/getAllClient', adminCntrl.getAllClient)
-  app.get('/api/staff/getAllStaff', adminCntrl.getAllStaff)
-  app.get('/api/client/details/:id', validator.params(adminModel.commonId), adminCntrl.getclientDetails)
-  app.put('/api/client/update/:id', validator.params(adminModel.commonId), adminCntrl.updateClient)
-  app.delete('/api/client/delete/:id', validator.params(adminModel.commonId), adminCntrl.deletedClient)
-  app.post('/api/client/uploadCsv', upload.single('file'), adminCntrl.uploadClientCsv)
+  app.post('/api/client/add', auth,validator.body(adminModel.addClient), adminCntrl.addClient)
+  app.get('/api/client/getAllClient',auth, adminCntrl.getAllClient)
+  app.get('/api/staff/getAllStaff',auth, adminCntrl.getAllStaff)
+  app.get('/api/client/details/:id', auth,validator.params(adminModel.commonId), adminCntrl.getclientDetails)
+  app.put('/api/client/update/:id', auth,validator.params(adminModel.commonId), adminCntrl.updateClient)
+  app.delete('/api/client/delete/:id',auth, validator.params(adminModel.commonId), adminCntrl.deletedClient)
+  app.post('/api/client/uploadCsv',upload.single('file'), adminCntrl.uploadClientCsv)
   /*** Client Routes's ends */
 
   /**Template Routes's starts */
   app.post('/api/template/add', auth, validator.body(adminModel.addTemplate), adminCntrl.addTemplate)
   app.put('/api/template/update/:id', auth, validator.params(adminModel.commonId), adminCntrl.updateTemplate)
-  app.get('/api/template/all', adminCntrl.getAllTemplates)
+  app.get('/api/template/all', auth,adminCntrl.getAllTemplates)
   /**Template Routes's ends */
 
 
   /**Assign Clients Routes Start */
   app.post('/api/client/assign', auth, validator.body(adminModel.assignClients), adminCntrl.assignClients)
-  app.get('/api/clientsatff/details/:id', validator.params(adminModel.commonId), adminCntrl.getclientStaffDetails);
+  app.get('/api/clientsatff/details/:id',auth, validator.params(adminModel.commonId), adminCntrl.getclientStaffDetails);
   /**Assign Clients Routes Ends */
 
 
   /**Admin Routes's starts */
-  app.get('/api/admin/dashboard', adminCntrl.getAdminDashboard);
-  app.get('/api/admin/documentmanagement', adminCntrl.getDocumentManagement);
+  app.get('/api/admin/dashboard',auth, adminCntrl.getAdminDashboard);
+  app.get('/api/admin/documentmanagement', auth,adminCntrl.getDocumentManagement);
   app.post('/api/admin/documentRequest', auth, adminCntrl.AdminDocumentRequest);
-  app.get('/api/admin/getAllClientsAdmin', adminCntrl.getAllClientsWithoutPagination);
-  app.get('/api/admin/getAssociatedSubCategory', adminCntrl.getAssociatedSubCategory);
-  app.get('/api/admin/getAllRequestedDocuments', adminCntrl.getAllRequestedDocuments);
+  app.get('/api/admin/getAllClientsAdmin', auth,adminCntrl.getAllClientsWithoutPagination);
+  app.get('/api/admin/getAssociatedSubCategory',auth, adminCntrl.getAssociatedSubCategory);
+  app.get('/api/admin/getAllRequestedDocuments',auth, adminCntrl.getAllRequestedDocuments);
   /**Admin Routes's ends */
 
   /** Email Templates Routes's starts */
-  app.post('/api/client/addEmailTemplate', adminCntrl.addEmailTemplate);
-  app.get('/api/client/getAllEmailTemplate', adminCntrl.getAllEmailTemplate);
+  app.post('/api/client/addEmailTemplate',auth, adminCntrl.addEmailTemplate);
+  app.get('/api/client/getAllEmailTemplate',auth, adminCntrl.getAllEmailTemplate);
   /** Email Templates Routes's ends */
 
   // get all document admin
 
-  app.get('/api/admin/document/title', adminCntrl.getAllDocumentTitle);
-  app.get('/api/admin/getAllReminderTemplates', adminCntrl.getAllReminderTemplates);
-  app.get('/api/admin/getAllScheduledReminder', adminCntrl.getAllScheduledList);
+  app.get('/api/admin/document/title',auth, adminCntrl.getAllDocumentTitle);
+  app.get('/api/admin/getAllReminderTemplates',auth, adminCntrl.getAllReminderTemplates);
+  app.get('/api/admin/getAllScheduledReminder',auth, adminCntrl.getAllScheduledList);
 
   /**Delete records */
   //app.get('/api/delete/document', clearDbCntrl.handleDelete);
@@ -79,8 +79,8 @@ module.exports = function (app, validator) {
   app.get('/api/logs/getAllLogs', auth, adminCntrl.getAllLogs);
 
   /**sub category update and delete starts */
-  app.patch('/api/update-subcategories/:id', adminCntrl.updateSubCategoryName);
-  app.delete('/api/delete-subcategories/:id', adminCntrl.deleteSubCategory);
+  app.patch('/api/update-subcategories/:id', auth,adminCntrl.updateSubCategoryName);
+  app.delete('/api/delete-subcategories/:id',auth, adminCntrl.deleteSubCategory);
   /**sub category update and delete ends */
 
 
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -15,8 +15,8 @@ const auth = require('../middleware/auth');
 
 module.exports = function (app, validator) {
    app.post('/api/admin/signin', validator.body(userModel.signinUser), userCntrl.signInUser);
-   app.get('/api/user/details/:id', validator.params(userModel.commonId), userCntrl.getUserDetails);
-   app.get('/api/user/getAllUser', userCntrl.getAllUser);
+   app.get('/api/user/details/:id', auth,validator.params(userModel.commonId), userCntrl.getUserDetails);
+   app.get('/api/user/getAllUser', auth,userCntrl.getAllUser);
    app.post('/api/staff/googleLogin', userCntrl.googleWithLogin);
    app.post('/api/admin/signup', validator.body(userModel.signupUser), userCntrl.signupUser);
    app.post('/api/role/add', userCntrl.addRole);
