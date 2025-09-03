e21169bfdc4bfdf2ac412bb912810d9c7d082278
shoyab <shoyabkhan.smartadata@gamil.com>
2025-09-03 11:48:15 +0530

    env url added

--- api/services/jwt.services.js
+++ api/services/jwt.services.js
@@ -3,16 +3,16 @@ const secret_key = "GHJI!@$%^&**"
 module.exports = {
 
   async issueJwtToken(payload) {
-    return jwt.sign(payload, secret_key, { expiresIn: '5000h' }); // 50 H expiration
+    return jwt.sign(payload, secret_key, { expiresIn: '5000h' }); // 228 days  expiration
   },
   async verifyJwtToken(token, cb) {
     return jwt.verify(token, secret_key, cb);
   },
   async linkToken(payload, expiresIn) {
-
+    let url = process.env.LINK_URL;
     const expiresInHours = expiresIn * 24;
     const token = jwt.sign(payload, secret_key, { expiresIn: `${expiresInHours}h` });
-    const finalUrl = `https://meanstack.smartdatainc.com:8076/client/token-handler/?token=${token}`
+    const finalUrl = `${url}/?token=${token}`
 
     return finalUrl;
   },
